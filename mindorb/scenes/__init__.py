4eb36b90fd9eb5abc3b342272cc9c2dc349c1230
Chris Post <chris@chrispost.com>
2017-12-31 00:44:48 -0800

    Support string-y scene names in `push_scene()`

--- mindorb/scenes/__init__.py
+++ mindorb/scenes/__init__.py
@@ -2,7 +2,16 @@
 
 from __future__ import division, absolute_import, print_function
 
+from enum import Enum
+
 from .solidcolor import SolidBlack
 from .solidcolor import SolidRed
 from .solidcolor import SolidGreen
 from .solidcolor import SolidBlue
+
+
+class AllScenes(Enum):
+    SolidBlack = SolidBlack
+    SolidRed = SolidRed
+    SolidGreen = SolidGreen
+    SolidBlue = SolidBlue
