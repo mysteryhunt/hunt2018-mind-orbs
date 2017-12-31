edec862b93b95a668640b41b5e6a9392d0976e91
Chris Post <chris@chrispost.com>
2017-12-31 00:17:31 -0800

    Respond to TERM also

--- mindorb/app.py
+++ mindorb/app.py
@@ -3,12 +3,24 @@
 
 from __future__ import division, absolute_import, print_function
 
+import signal
+
 from mindorb.commandreceiver import CommandReceiver
 from mindorb.scenecontrol import SceneManager
 
 
+threads = []
+
+
+def shutdown(signum, _):
+    print("Signal {} received! -> sending shutdown...".format(signum))
+    for t in threads:
+        t.shutdown = True
+
+
 def main():
-    threads = []
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
 
     scene_manager = SceneManager(20)
     threads.append(scene_manager)
@@ -20,14 +32,9 @@ def main():
 
     # TODO: do this all less shitty maybe?
     while len([t for t in threads if t.is_alive()]) > 0:
-        try:
-            for t in threads:
-                if t.is_alive():
-                    t.join(1)
-        except KeyboardInterrupt:
-            print("KeyboardInterrupt received! -> sending shutdown...")
-            for t in threads:
-                t.shutdown = True
+        for t in threads:
+            if t.is_alive():
+                t.join(1)
 
 
 if __name__ == '__main__':
